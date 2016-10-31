a2595d17603d64b40761f2cc65d7cf6c4278673e
Jasmine <jasminelee@berkeley.edu>
2016-10-30 23:17:50 -0700

    add guess letters and restart game after winning functionality

--- Hangman/Hangman/GameViewController.swift
+++ Hangman/Hangman/GameViewController.swift
@@ -8,6 +8,21 @@
 
 import UIKit
 
+extension String {
+    subscript(i: Int) -> String {
+        guard i >= 0 && i < characters.count else { return "" }
+        return String(self[index(startIndex, offsetBy: i)])
+    }
+    subscript(range: Range<Int>) -> String {
+        let lowerIndex = index(startIndex, offsetBy: max(0,range.lowerBound), limitedBy: endIndex) ?? endIndex
+        return substring(with: lowerIndex..<(index(lowerIndex, offsetBy: range.upperBound - range.lowerBound, limitedBy: endIndex) ?? endIndex))
+    }
+    subscript(range: ClosedRange<Int>) -> String {
+        let lowerIndex = index(startIndex, offsetBy: max(0,range.lowerBound), limitedBy: endIndex) ?? endIndex
+        return substring(with: lowerIndex..<(index(lowerIndex, offsetBy: range.upperBound - range.lowerBound + 1, limitedBy: endIndex) ?? endIndex))
+    }
+}
+
 class GameViewController: UIViewController {
 
 
@@ -26,6 +41,7 @@ class GameViewController: UIViewController {
     @IBOutlet weak var puzzleString: UILabel!
     @IBOutlet weak var guess: UITextField!
     @IBOutlet weak var hangmanImage: UIImageView!
+
     @IBOutlet weak var incorrectGuesses: UILabel!
     @IBOutlet weak var numIncorrectGuesses: UILabel!
     
@@ -35,10 +51,20 @@ class GameViewController: UIViewController {
 
         // Do any additional setup after loading the view.
         let hangmanPhrases = HangmanPhrases()
-        let phrase = hangmanPhrases.getRandomPhrase()
+        phrase = hangmanPhrases.getRandomPhrase()
+        print(phrase)
         incorrectGuesses.text = ""
-        numIncorrectGuesses.text = String(incorrectGuesses.text!.characters.count)
-        puzzleString.text = String(repeating: "-", count: (phrase?.characters.count)!)
+        numIncorrectGuesses.text = String(0)
+        var puzzleStringText = ""
+        for char:Character in phrase.characters {
+            if char == " " {
+                puzzleStringText += " "
+            } else {
+                puzzleStringText += "-"
+            }
+        }
+        puzzleString.text = puzzleStringText
+        drawHangman()
     }
 
     override func didReceiveMemoryWarning() {
@@ -53,24 +79,57 @@ class GameViewController: UIViewController {
     }
 
     
+    
     func updateGuess() {
-        let guessedLetter:String = "\(guess.text?.characters.first)"
-        
+        let gl = guess.text?.characters.first
+        let guessedLetter = "\(gl!)".uppercased()
         if phrase.contains(guessedLetter) {
-            let displayString = puzzleString.text
-            let newStr = displayString?.replacingOccurrences(of: "-", with: guessedLetter, options: .literal, range: nil)
-            puzzleString.text = newStr
+            var displayStr = ""
+            
+            for (index, char) in (puzzleString.text?.characters.enumerated())! {
+                if phrase[index] == guessedLetter {
+                    displayStr += phrase[index]
+                } else if "\(char)" == "-" {
+                    displayStr += "-"
+                } else {
+                    displayStr += phrase[index]
+                }
+            }
+            puzzleString.text = displayStr
         } else {
             let newStr:String = incorrectGuesses.text! + guessedLetter
             incorrectGuesses.text = newStr
-            let newNumIncorrect = Int(numIncorrectGuesses.text!) += 1
-            numIncorrectGuesses.text = "\(newNumIncorrect)"
+            var newNumIncorrect = Int(numIncorrectGuesses.text!)
+            newNumIncorrect! += 1
+            numIncorrectGuesses.text = "\(newNumIncorrect!)"
+        }
+        checkGameStatus()
+    }
+    
+    func checkGameStatus() {
+        let victory = !(puzzleString.text?.contains("-"))!
+        if victory {
+            let alertController = UIAlertController(title: "You won!", message:
+                "Want to play again?", preferredStyle: UIAlertControllerStyle.alert)
+            alertController.addAction(UIAlertAction(title: "Yes", style: UIAlertActionStyle.default,handler: resetGame))
+            alertController.addAction(UIAlertAction(title: "No", style: UIAlertActionStyle.default,handler: backToHome))
+            self.present(alertController, animated: true, completion: nil)
         }
     }
     
+    func resetGame(alert: UIAlertAction!) {
+        viewDidLoad()
+        viewWillAppear(true)
+    }
+    
+    func backToHome(alert: UIAlertAction!) {
+        let mapViewControllerObj = self.storyboard?.instantiateViewController(withIdentifier: "HomeController")
+        self.navigationController?.present(mapViewControllerObj!, animated: true, completion: nil)
+    }
+    
     func drawHangman() {
-        let numIncorrect:Int = Int(numIncorrectGuesses.text!)!
-        switch numIncorrect {
+        let numIncorrect = Int(numIncorrectGuesses.text!)
+        switch numIncorrect! {
         case 0:
             hangmanImage.image = hm1
         case 1:
